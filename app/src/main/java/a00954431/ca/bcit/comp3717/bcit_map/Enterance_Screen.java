a327a407828427d7894935ee7efc8790cc351aea
ErikaLearned <erikamoatemail@gmail.com>
2016-10-20 23:30:12 -0700

    Fix enterance spelling mistake

--- app/src/main/java/a00954431/ca/bcit/comp3717/bcit_map/Enterance_Screen.java
+++ app/src/main/java/a00954431/ca/bcit/comp3717/bcit_map/Enterance_Screen.java
@@ -3,11 +3,11 @@ package a00954431.ca.bcit.comp3717.bcit_map;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Enterence_Screen extends AppCompatActivity {
+public class Enterance_Screen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_enterence__screen);
+        setContentView(R.layout.activity_enterance__screen);
     }
 }
