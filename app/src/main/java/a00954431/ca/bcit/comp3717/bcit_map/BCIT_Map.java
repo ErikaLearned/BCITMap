c283057a6b0984acfa5dc36c3ff4d7253896d39e
Erika Moate <erikamoatemail@gmail.com>
2016-11-17 10:20:02 -0800

    Adjust SE12 floor 2 cooridinates to fit the floor plan to the map better.
    Criteria for complete-ness: The exterior stairwells mostly overlap. The north and south stairwells fit best. The east stairwell is a little off.

--- app/src/main/java/a00954431/ca/bcit/comp3717/bcit_map/BCIT_Map.java
+++ app/src/main/java/a00954431/ca/bcit/comp3717/bcit_map/BCIT_Map.java
@@ -162,8 +162,8 @@ public class BCIT_Map extends FragmentActivity implements OnMapReadyCallback {
                 GroundOverlayOptions se12OverlayOption = new GroundOverlayOptions()
                         .image(BitmapDescriptorFactory.fromResource(R.drawable.se12f2m))
                         .positionFromBounds(new LatLngBounds(
-                                new LatLng(49.249406, -123.002147),     // South west corner
-                                new LatLng(49.250502, -123.001133)      // North east corner
+                                new LatLng(49.249360, -123.002164),     // South west corner
+                                new LatLng(49.250528, -123.001075)      // North east corner
                         ));
                 groundOverlaysSE[7] =  mMap.addGroundOverlay(se12OverlayOption);
                 break;
