470e771e2a67fa00ee6f6aa5e516554d943bd803
jacobmcphail <jacobmcphail@gmail.com>
2016-11-23 17:44:17 -0800

    Get location somewhat working

--- app/src/main/java/a00954431/ca/bcit/comp3717/bcit_map/Directions_Menu.java
+++ app/src/main/java/a00954431/ca/bcit/comp3717/bcit_map/Directions_Menu.java
@@ -1,11 +1,20 @@
 package a00954431.ca.bcit.comp3717.bcit_map;
 
+import android.*;
+import android.Manifest;
 import android.app.ActionBar;
 import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -28,18 +37,35 @@ import android.widget.LinearLayout;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.LatLng;
 
 import java.sql.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class Directions_Menu extends AppCompatActivity {
+public class Directions_Menu extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
 
     ArrayList<String> roomList = new ArrayList<String>();
     ArrayList<String> roomList_FilteredTo = new ArrayList<String>();
     ArrayList<String> roomList_FilteredFrom = new ArrayList<String>();
+    ArrayList<Node> dbList;
+
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+
+    private LatLng currentLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,10 +74,25 @@ public class Directions_Menu extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                // The next two lines tell the new client that “this” current class will handle connection stuff
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                //fourth line adds the LocationServices API endpoint from GooglePlayServices
+                .addApi(LocationServices.API)
+                .build();
+
+        // Create the LocationRequest object
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(10 * 1000)        // 10 seconds, in milliseconds
+                .setFastestInterval(1 * 1000); // 1 second, in milliseconds
+
         final Button dirButt = (Button) findViewById(R.id.getDirections);
         dirButt.setEnabled(false);
         final CheckBox outdoor = (CheckBox) findViewById(R.id.checkBoxOutdoors);
         final CheckBox locationcheck = (CheckBox) findViewById(R.id.checkboxlocation);
+        final Spinner areaSpinner = (Spinner) findViewById(R.id.detectspinner);
         final EditText fromText = (EditText)findViewById(R.id.fromSearch);
         final EditText toText = (EditText)findViewById(R.id.toSearch);
         final LinearLayout searchlayout = (LinearLayout)findViewById(R.id.searchlayout);
@@ -59,8 +100,9 @@ public class Directions_Menu extends AppCompatActivity {
 
         locationcheck.setVisibility(View.GONE);
         searchlayout.setVisibility(View.GONE);
+        areaSpinner.setVisibility(View.GONE);
 
-        ArrayList<Node> dbList = NodeDir.mapDB.getAllNodes();
+        dbList = NodeDir.mapDB.getAllNodes();
 
         for(Node node : dbList) {
             if (!node.roomNum.equals("")) {
@@ -88,12 +130,78 @@ public class Directions_Menu extends AppCompatActivity {
             }
         });
 
+
         locationcheck.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
 
             @Override
             public void onCheckedChanged(CompoundButton buttonView,boolean isChecked) {
-                // TODO
-                Toast.makeText(Directions_Menu.this, "BOX = " + isChecked, Toast.LENGTH_SHORT).show();
+                if (isChecked) {
+                   runLocation();
+                    areaSpinner.setVisibility(View.VISIBLE);
+                } else {
+                    areaSpinner.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.detect_areas, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        areaSpinner.setAdapter(adapter);
+        areaSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position > 0) {
+                    Node bestNode = null;
+                    double bestSum = Double.POSITIVE_INFINITY;
+                    LatLng local = currentLocation;
+
+                    for(Node node : dbList) {
+                        if (node.floor < 6) {
+                            if (node.floor != position) {
+                                continue;
+                            }
+                        } else if (position == 6 && !node.outside) {
+                            continue;
+                        }
+                        double sum =  Math.abs(node.loc.latitude - local.latitude) + Math.abs(node.loc.longitude - local.longitude);
+                        if (sum < bestSum) {
+                            bestSum = sum;
+                            bestNode = node;
+                        }
+                    }
+
+                    // TODO
+
+                    if (bestNode == null) {
+                        return;
+                    }
+
+                    String best = "";
+
+                    if (!bestNode.roomNum.equals("")) {
+                        best = bestNode.building + "-" + bestNode.roomNum;
+                    } else if (!bestNode.roomName.equals("")) {
+                        best = bestNode.building + "-" + bestNode.roomName;
+                    } else {
+                        best = bestNode.building + "-" + bestNode.key;
+                    }
+
+                    View v = getCurrentFocus();
+                    if (v.equals(findViewById(R.id.fromSearch))) {
+                        fromText.setText(best);
+                        fromText.clearFocus();
+                    } else if (v.equals(findViewById(R.id.toSearch))) {
+                        toText.setText(best);
+                        toText.clearFocus();
+                    }
+                    areaSpinner.setVisibility(View.GONE);
+                    InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                    setStartButton();
+                }
+            }
+            public void onNothingSelected(AdapterView<?> arg0) {
+                // Do Nothing
             }
         });
 
@@ -279,7 +387,6 @@ public class Directions_Menu extends AppCompatActivity {
         } else {
             Intent myIntent = new Intent(getApplicationContext(), Entrance_Screen.class);
             startActivityForResult(myIntent, 0);
-            finish();
             return  true;
         }
     }
@@ -325,6 +432,7 @@ public class Directions_Menu extends AppCompatActivity {
                     });
             alertDialog.show();
         } else {
+            // TODO
             CheckBox outdoor = (CheckBox) findViewById(R.id.checkBoxOutdoors);
             Intent intent = new Intent(this, BCIT_Map.class);
             intent.putExtra("fromLocation", searchFrom);
@@ -342,5 +450,137 @@ public class Directions_Menu extends AppCompatActivity {
         }
         return false;
     }
+
+    public void runLocation() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED &&  ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED && mGoogleApiClient.isConnected()) {
+            Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+            if (location == null) {
+                LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+
+            } else {
+                //If everything went fine lets get latitude and longitude
+                double currentLatitude = location.getLatitude();
+                double currentLongitude = location.getLongitude();
+
+                currentLocation = new LatLng(currentLatitude, currentLongitude);
+            }
+        } else {
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[] { android.Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION },
+                    1
+            );
+            CheckBox locationcheck = (CheckBox) findViewById(R.id.checkboxlocation);
+            locationcheck.setChecked(false);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED) {
+                    Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+                    if (location == null) {
+                        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+
+                    } else {
+                        //If everything went fine lets get latitude and longitude
+                        double currentLatitude = location.getLatitude();
+                        double currentLongitude = location.getLongitude();
+
+                        currentLocation = new LatLng(currentLatitude, currentLongitude);
+                    }
+                } else {
+                    Toast.makeText(this, "Permission denied cant get your location!", Toast.LENGTH_SHORT).show();
+                    CheckBox locationcheck = (CheckBox) findViewById(R.id.checkboxlocation);
+                    locationcheck.setChecked(false);
+                }
+                return;
+            }
+        }
+    }
+
+    /**
+     * If connected get lat and long
+     *
+     */
+    @Override
+    public void onConnected(Bundle bundle) {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED &&  ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+            if (location == null) {
+                LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+
+            } else {
+                //If everything went fine lets get latitude and longitude
+                double currentLatitude = location.getLatitude();
+                double currentLongitude = location.getLongitude();
+
+                currentLocation = new LatLng(currentLatitude, currentLongitude);
+            }
+        }
+    }
+
+
+    @Override
+    public void onConnectionSuspended(int i) {}
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+        Log.e("Error", "Location services connection failed with code " + connectionResult.getErrorCode());
+    }
+
+    /**
+     * If locationChanges change lat and long
+     *
+     *
+     * @param location
+     */
+    @Override
+    public void onLocationChanged(Location location) {
+        double currentLatitude = location.getLatitude();
+        double currentLongitude = location.getLongitude();
+
+        currentLocation = new LatLng(currentLatitude, currentLongitude);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //Now lets connect to the API
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.v(this.getClass().getSimpleName(), "onPause()");
+
+        //Disconnect from API onPause()
+        if (mGoogleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+            mGoogleApiClient.disconnect();
+        }
+
+
+    }
+
+
+
+
+
+
 
 }
