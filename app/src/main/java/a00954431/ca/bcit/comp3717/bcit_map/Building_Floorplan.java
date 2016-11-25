13b69c8a126918b25f82d90239b169f0af02cb46
Erika Moate <erikamoatemail@gmail.com>
2016-11-25 12:39:47 -0800

    Adjust SE12 floorplans. Temp. changed map to tarrain. NOTE: Change map back to normal in the bcit_map layout xml.

--- app/src/main/java/a00954431/ca/bcit/comp3717/bcit_map/Building_Floorplan.java
+++ app/src/main/java/a00954431/ca/bcit/comp3717/bcit_map/Building_Floorplan.java
@@ -18,16 +18,16 @@ public class Building_Floorplan {
         plans = new TreeMap<String, GroundOverlayOptions>();
 
         // SE12 - floors 1 to 4
-        buildFloorplan(new LatLng(49.249457, -123.002164),
-                       new LatLng(49.250539, -123.001253),
+        buildFloorplan(new LatLng(49.249459, -123.002017),
+                       new LatLng(49.250562, -123.001254),
                        R.drawable.se12f1m,
                        "se12f1m");
         buildFloorplan(new LatLng(49.249457, -123.002164),
-                       new LatLng(49.250528, -123.001075),
+                       new LatLng(49.250526, -123.001041),
                        R.drawable.se12f2m,
                        "se12f2m");
         buildFloorplan(new LatLng(49.249457, -123.001834),
-                       new LatLng(49.250505, -123.001316),
+                       new LatLng(49.250503, -123.001262),
                        R.drawable.se12f3m,
                        "se12f3m");
         buildFloorplan(new LatLng(49.249457, -123.001834),
