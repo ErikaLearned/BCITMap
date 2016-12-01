5ebbcd6205570158d58ce9d251f788005b6a0e83
Erika Moate <erikamoatemail@gmail.com>
2016-11-30 20:54:41 -0800

    Set up basic Schedule_Day activity to be called by Schedule.

--- app/src/main/java/a00954431/ca/bcit/comp3717/bcit_map/Schedule_Day.java
+++ app/src/main/java/a00954431/ca/bcit/comp3717/bcit_map/Schedule_Day.java
@@ -1,6 +1,5 @@
 package a00954431.ca.bcit.comp3717.bcit_map;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,25 +8,23 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
-public class Schedule extends AppCompatActivity {
+public class Schedule_Day extends AppCompatActivity {
     private ListView listView;
     private String[] display;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_schedule);
+        setContentView(R.layout.activity_schedule__day);
+        String day = getIntent().getStringExtra("Day");
 
         // Get ListView object and populate it ((DUMMY DATA ATM))
-        listView = (ListView) findViewById(R.id.schedule_list);
+        listView = (ListView) findViewById(R.id.scheduleday_list);
 
-        display = new String[] { "Monday",
-                "Tuesday",
-                "Wednesday",
-                "Thursday",
-                "Friday",
-                "Saturday",
-                "Sunday"
+        display = new String[] { day,
+                "Current to Room1",
+                "Current to Room2",
+                "All Day"
         };
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.schedule_textview, display);
@@ -41,14 +38,11 @@ public class Schedule extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id) {
                 // ListView Clicked item value
-                String  day   = (String) listView.getItemAtPosition(position);
+                String  itemValue    = (String) listView.getItemAtPosition(position);
                 // Show Alert
                 Toast.makeText(getApplicationContext(),
-                        "ListItem : " + day , Toast.LENGTH_LONG)
+                        "ListItem : " +itemValue , Toast.LENGTH_LONG)
                         .show();
-                Intent intent = new Intent(Schedule.this, Schedule_Day.class);
-                intent.putExtra("Day", day);
-                startActivity(intent);
             }
         });
     }
